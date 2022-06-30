a99e943046e48b111c1b04c970829102e2d6c54b
KosukeShibata <ssibataa12@gmail.com>
2022-06-30 21:45:41 +0900

    ツイートの登録・編集・削除機能の追加

--- resources/views/tweet/update.blade.php
+++ resources/views/tweet/update.blade.php
@@ -10,11 +10,24 @@
 <body>
     <h1>つぶやきを編集する</h1>
     <div>
-    </div>
-    <div>
-    @foreach($tweets as $tweet)
-        <p>{{ $tweet->content }}</p>
-    @endforeach
+        <a href="{{ route('tweet.index') }}">戻る</a>
+        <p>投稿フォーム</p>
+        @if (session('feedback.success'))
+            <p style="color: green;">{{ session('feedback.success') }}</p>
+        @endif
+        <form action="{{ route('tweet.update.put', ['tweetId' => $tweet->id]) }}" method="post">
+            @method('PUT')
+            @csrf
+            <label for="tweet-content">つぶやき</label>
+            <span>140文字まで</span>
+            <textarea id="tweet-content" name="tweet" placeholder="つぶやきを入力">
+                {{ $tweet->content }}
+            </textarea>
+            @error('tweet')
+            <p style="color: red;">{{ message }}</p>
+            @enderror
+            <button type="submit">編集</button>
+        </form>
     </div>
 </body>
 </html>
