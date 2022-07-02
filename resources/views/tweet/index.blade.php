47e6f7a4b7f67c3839dba2b3b0a4e34143341be0
KosukeShibata <ssibataa12@gmail.com>
2022-07-02 14:40:30 +0900

    tweetしたユーザーのみがツイートを変更できるように変更

--- resources/views/tweet/index.blade.php
+++ resources/views/tweet/index.blade.php
@@ -30,7 +30,8 @@
     <div>
     @foreach($tweets as $tweet)
         <details>
-            <summary>{{ $tweet->content }}</summary> by {{ $tweet->user->name }}
+            <summary>{{ $tweet->content }} by {{ $tweet->user->name }} </summary>
+            @if(\Illuminate\Support\Facades\Auth::id() === $tweet->user_id)
             <div>
                 <a href="{{ route('tweet.update.index', ['tweetId' => $tweet->id]) }}">編集</a>
                 <form action="{{ route('tweet.delete', ['tweetId' => $tweet->id]) }}" method="post">
@@ -39,6 +40,9 @@
                     <button type="submit">削除</button>
                 </form>
             </div>
+            @else
+                編集できません
+            @endif
         </details>
     @endforeach
     </div>
